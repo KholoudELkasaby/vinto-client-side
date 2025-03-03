5a451ff4f353bf30b273bd38d8b323b7370d4000
KholoudELkasaby <lilelksaby@gmail.com>
2025-03-03 19:14:14 +0200

    add home and details components with basic structure and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HomeComponent],
 })
 export class AppComponent {
   title = 'vinto-client-side';
