51581dc4dee42d2f0d05471a85afdde6210b2f92
KholoudELkasaby <lilelksaby@gmail.com>
2025-03-03 15:40:42 +0200

    add custom color palette to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray": "#E2E0E0",
+        "gray-color": "#A5A5A5",
+        "light-color": "#9E9292",
+        "background-color": "#F5F1EE",
+      },
+    },
   },
   plugins: [],
 };
