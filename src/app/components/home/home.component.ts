0b13e0df8923afb2bdfaf6351d9871ab74835359
KholoudELkasaby <lilelksaby@gmail.com>
2025-03-03 22:03:20 +0200

    add category component to home page with layout and styles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { CategoryComponent } from './category/category.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CategoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
