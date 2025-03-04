ac3e591007153525c82c3db910c4b258ec9decdc
KholoudELkasaby <lilelksaby@gmail.com>
2025-03-04 19:49:49 +0200

    add tabs component with layout and functionality; update home component to include tabs section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CategoryComponent } from './category/category.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CategoryComponent],
+  imports: [HeaderComponent, CategoryComponent, TabsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/components/home/tabs/tabs.component.ts
+++ src/app/components/home/tabs/tabs.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabs.component.html',
-  styleUrl: './tabs.component.css'
+  styleUrl: './tabs.component.css',
 })
 export class TabsComponent {
+  activeTab: string = 'New Arrivals';
 
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
