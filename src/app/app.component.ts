80fa585bbe2f1a2a9b8fb73c455c6e3e04f03d49
KholoudELkasaby <lilelksaby@gmail.com>
2025-03-09 00:53:48 +0200

    add navbar component; update app component to include navbar and adjust details section layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HomeComponent, DetailsComponent],
+  imports: [HomeComponent, DetailsComponent, NavbarComponent],
 })
 export class AppComponent {
   title = 'vinto-client-side';
