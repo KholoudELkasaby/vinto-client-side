a471e9ec2c227445c9008e97460ff828c385742e
KholoudELkasaby <lilelksaby@gmail.com>
2025-03-08 04:35:21 +0200

    refactor details-info component to dynamically render color and size options; implement active selection functionality

--- src/app/components/details/details-info/details-info.component.ts
+++ src/app/components/details/details-info/details-info.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { AddToCartBtnComponent } from '../add-to-cart-btn/add-to-cart-btn.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details-info',
-  imports: [AddToCartBtnComponent],
+  imports: [AddToCartBtnComponent, CommonModule],
   templateUrl: './details-info.component.html',
   styleUrl: './details-info.component.css',
 })
-export class DetailsInfoComponent {}
+export class DetailsInfoComponent {
+  activeColor: number = 0;
+  activeSize: number = 2;
+
+  colors: string[] = ['#A46016', '#000000', '#9E9292'];
+  sizes: string[] = ['S', 'M', 'L', 'XL'];
+
+  setActive(type: string, index: number) {
+    if (type === 'color') {
+      this.activeColor = index;
+    } else if (type === 'size') {
+      this.activeSize = index;
+    }
+  }
+}
