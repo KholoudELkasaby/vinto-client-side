7d9fd7490572920e7e11da5d18a14f766cb84005
mohamedhw <mohamedhw2000@gmail.com>
2025-03-20 01:54:24 +0200

    feat: added the cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,28 +1,54 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CartService {
-  private readonly URL = "http://localhost:4000/api/cart";
+
+
+  private URL = "http://localhost:4000/api/cart";
+  private historySubject = new BehaviorSubject<any[]>([]);
+  private historyData: any[] = [];
 
   constructor(public http: HttpClient) { }
 
-  getAllCarts() {
-    return this.http.get(this.URL)
+  getHistory(userId: string): Observable<any> {
+    if (this.historyData.length === 0) {
+      return this.http.get(`${this.URL}/history/${userId}`).pipe(
+        map((data: any) => {
+          this.historyData = data.data.carts;
+          this.historySubject.next(this.historyData);
+
+          return data;
+        })
+      );
+    }
+    return this.historySubject.asObservable();
   }
 
-  getCart(id: number) {
-    return this.http.get(this.URL + '/' + id)
+
+  filterHistory(status: string): any[] {
+    if (status === 'All orders') return this.historyData;
+
+    const statusMap: { [key: string]: string } = {
+      'Completed': 'completed',
+      'Cancelled': 'cancelled',
+      'Inprogress': 'inprogress',
+      'Summary': 'summary'
+    };
+
+    return this.historyData.filter(item =>
+      status === 'Summary' ? item.summaryExists : item.status === statusMap[status]
+    );
   }
 
-  createCart(cart: any) {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    });
-    return this.http.post(this.URL, cart, { headers })
+  getCart(userId: string) {
+    console.log(`${this.URL}/${userId}`)
+    return this.http.get(`${this.URL}/${userId}`)
   }
-
 }
