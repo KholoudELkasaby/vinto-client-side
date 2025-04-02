c7be64436c12343add6be30e0f6957b0fe25e252
KholoudELkasaby <lilelksaby@gmail.com>
2025-04-02 15:52:18 +0200

    refactor: clean up code formatting and improve readability in productitem and products components

--- src/app/productitem/productitem.component.ts
+++ src/app/productitem/productitem.component.ts
@@ -1,6 +1,6 @@
 import { ProductDetailsComponent } from './../product-details/product-details.component';
 import { ProductsComponent } from './../products/products.component';
-import { Component, input, Input , OnInit ,SimpleChanges } from '@angular/core';
+import { Component, input, Input, OnInit, SimpleChanges } from '@angular/core';
 import { FilteredImageComponent } from '../filtered-image/filtered-image.component';
 import { FormsModule } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
@@ -8,48 +8,43 @@ import { FormControl, FormControlName, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 
-
 @Component({
   selector: 'app-productitem',
-  standalone:true,
-  imports: [FormsModule, RatingModule , CommonModule ,RouterModule] , 
+  standalone: true,
+  imports: [FormsModule, RatingModule, CommonModule, RouterModule],
   templateUrl: './productitem.component.html',
-  styleUrl: './productitem.component.css'
+  styleUrl: './productitem.component.css',
 })
 export class ProductitemComponent {
-@Input() products:any;
-@Input() controlName: any;
-@Input() rate:number=0 ;
-@Input() id:any;
+  @Input() products: any;
+  @Input() controlName: any;
+  @Input() rate: number = 0;
+  @Input() id: any;
 
-getStarClass(index: number): any {
-  const fullStars = Math.floor(this.rate);
-  const decimalPart = this.rate - fullStars;
-  // console.log( "full"+fullStars);
-// console.log("rate" , this.rate)
-// console.log(decimalPart);
-// console.log(index);
-// console.log("*******************************")
+  getStarClass(index: number): any {
+    const fullStars = Math.floor(this.rate);
+    const decimalPart = this.rate - fullStars;
+    // console.log( "full"+fullStars);
+    // console.log("rate" , this.rate)
+    // console.log(decimalPart);
+    // console.log(index);
+    // console.log("*******************************")
 
-
-  if (index < fullStars) {
-    return 'full-star'; 
+    if (index < fullStars) {
+      return 'full-star';
+    }
+    if (decimalPart > 0) {
+      if (decimalPart > 0 && decimalPart <= 0.25) return 'quarter-star';
+      if (decimalPart > 0.25 && decimalPart <= 0.5) return 'half-star';
+      if (decimalPart > 0.5 && decimalPart <= 0.75) return 'three-quarter-star';
+      if (decimalPart > 0.75) return 'full-star';
+    } else {
+      return 'empty-star';
+    }
   }
-    if (decimalPart>0) {
-    if (decimalPart > 0 && decimalPart <= 0.25) return 'quarter-star';
-    if (decimalPart > 0.25 && decimalPart <= 0.5) return 'half-star';
-    if (decimalPart > 0.5 && decimalPart <= 0.75) return 'three-quarter-star';
-    if (decimalPart > 0.75 ) return 'full-star';
+  liked: boolean = false; // Track like state
+
+  toggleLike(): void {
+    this.liked = !this.liked;
   }
-  else{
-    return 'empty-star';
-
-  }
-}
-liked: boolean = false; // Track like state
-
-toggleLike(): void {
-  this.liked = !this.liked;
-}
-
 }
