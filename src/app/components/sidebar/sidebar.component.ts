5ff9421b359e10ce3d28606a4d27153d9afcb899
banseh <banseh@gmail.com>
2025-04-05 19:11:48 +0200

    edit category &price

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -105,41 +105,26 @@ export class SidebarComponent {
   }
 
   // SG:any;
-
   press() {
     this.isDropdownVisible = !this.isDropdownVisible;
-    const dropdownHelper = document.getElementById(
-      'dropdownHelper'
-    ) as HTMLDivElement | null;
-    if (dropdownHelper) {
-      dropdownHelper.style.display = this.isDropdownVisible ? 'block' : 'none';
-
-      this.x1 =
-        this.x1 == 'M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7'
-          ? 'm1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1'
-          : 'M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7';
-      // console.log(this.SG) ;
-      // this.SG.tra = "transform 0.3s ease-in-out";
-      // this.SG.style.transform = this.isDropdownVisible ? "rotate(180deg)" : "rotate(0deg)";
-    }
-    // console.log(",,,,,,,,,,,,,,,,");
+    this.x1 =
+      this.x1 == 'M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7'
+        ? 'm1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1'
+        : 'M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7';
   }
+  
 
   press2() {
     this.isDropdownVisible2 = !this.isDropdownVisible2;
-    const dropdownHelperp = document.getElementById(
-      'dropdownHelperp'
-    ) as HTMLDivElement | null;
-    if (dropdownHelperp) {
-      dropdownHelperp.style.display = this.isDropdownVisible2
-        ? 'block'
-        : 'none';
-      this.x2 =
-        this.x2 == 'M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7'
-          ? 'm1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1'
-          : 'M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7';
-    }
-    // console.log(",,,,,,,,,,,,,,,,");
+
+    // Toggle the icon based on dropdown visibility
+    this.x2 = this.isDropdownVisible2
+      ? 'm1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1' // rotated icon (upwards)
+      : 'M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7'; // original icon (downwards)
+    
+    // Log to check the current state
+    console.log("Dropdown visibility:", this.isDropdownVisible2);
+    console.log("Icon state:", this.x2);
   }
 
   state: boolean = false;
