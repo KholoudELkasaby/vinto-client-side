d3bdfb264b5931eac8f9599a7d60fc3faa0f2db6
KholoudELkasaby <lilelksaby@gmail.com>
2025-04-09 22:23:10 +0200

    feat: enable OnPush change detection strategy for category and navbar components

--- src/app/components/home/category/category.component.ts
+++ src/app/components/home/category/category.component.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { Category } from '../../../models/category.model';
 import { CategoryService } from '../../../services/category.service';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-category',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, NgxSkeletonLoaderModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css',
@@ -14,13 +22,17 @@ export class CategoryComponent implements OnInit {
   @Output() Category = new EventEmitter<void>();
   categories: Category[] = [];
   isLoading: boolean = true;
-  constructor(private categoryService: CategoryService) {}
+  constructor(
+    private categoryService: CategoryService,
+    private cdr: ChangeDetectorRef
+  ) {}
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe({
       next: (response) => {
         this.categories = response.data.categories;
         console.log('Fetched Categories:', this.categories);
         this.isLoading = false;
+        this.cdr.markForCheck();
       },
       error: (err) => {
         console.error('Error fetching categories:', err);
